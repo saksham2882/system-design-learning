5e37b080beedc56f8f11fd73bc2c974cefd75454
Saksham Agrahari <agrahari0899@gmail.com>
2026-08-19 00:10:09 +0530

    add OOP abstraction & encapsulation example

---
+++ src/lecture_02_OOPs/Car.java
@@ -0,0 +1,9 @@
+package lecture_02_OOPs;
+
+public interface Car {
+    public void startEngine();
+    public void stopEngine();
+    public void accelerate();
+    public void brake();
+    public void shiftGear(int gear);
+}
